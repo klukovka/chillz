7877bf71531178d057182cbb48423a76bfa65a45
Yuliia Koba <ykoba@clipmoney.com>
2025-02-23 15:51:54 +0200

    fix footer

--- src/components/FooterNavigationItem.jsx
+++ src/components/FooterNavigationItem.jsx
@@ -1,10 +1,12 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classes from "./Footer.module.css";
 
 export default function FooterNavigationBarItem({ children, to }) {
   return (
     <li className={classes["footer-nav__item"]}>
-      <NavLink to={to}>{children}</NavLink>
+      <Link to={to} target="_blank">
+        {children}
+      </Link>
     </li>
   );
 }
