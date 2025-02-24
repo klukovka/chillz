e3296d7f6e9b0cef573c9fc55b99027f01eee207
Yuliia Koba <ykoba@clipmoney.com>
2025-02-24 22:59:42 +0200

    small fixes

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import MainNavigationBar from "../components/MainNavigationBar";
-import PinAnimation from "../components/PinAnimation";
 
 export default function RootLayout() {
   return (
@@ -10,9 +9,7 @@ export default function RootLayout() {
       <main>
         <Outlet />
       </main>
-      <PinAnimation>
-        <Footer />
-      </PinAnimation>
+      <Footer />
     </>
   );
 }
--- src/pages/about/components/WhatDoWeDo.jsx
+++ src/pages/about/components/WhatDoWeDo.jsx
@@ -13,70 +13,71 @@ export default function WhatDoWeDo() {
         <div className={classes["what-do-we-do"]}>
           <PhysicsContainer
             className={classes["physics-container"]}
-            height={classes["what-do-we-do"].height}
+            // TODO: Update height
+            height={458}
             elements={[
               {
                 width: 260,
                 height: 64,
                 text: "brand identity",
                 offset: 650,
-                fallHeight: 300,
+                fallHeight: 400,
               },
               {
                 width: 232,
                 height: 64,
                 text: "graphic design",
                 offset: 500,
-                fallHeight: 250,
+                fallHeight: 350,
               },
               {
                 width: 186,
                 height: 64,
                 text: "visual\ncontent",
                 offset: 350,
-                fallHeight: 280,
+                fallHeight: 380,
               },
               {
                 width: 218,
                 height: 64,
                 text: "smm\nstrategy",
-                offset: 120,
-                fallHeight: 50,
+                offset: 150,
+                fallHeight: 150,
               },
               {
                 width: 182,
                 height: 64,
                 text: "influ\nmarketing",
                 offset: 285,
-                fallHeight: 80,
+                fallHeight: 280,
               },
               {
                 width: 132,
                 height: 64,
                 text: "SEO/SEM",
                 offset: 350,
-                fallHeight: 150,
+                fallHeight: 250,
               },
               {
                 width: 220,
                 height: 64,
                 text: "social media\noptimization",
                 offset: 500,
-                fallHeight: 80,
+                fallHeight: 180,
               },
               {
                 width: 190,
                 height: 64,
                 text: "targeting",
                 offset: 600,
-                fallHeight: 100,
+                fallHeight: 200,
               },
               {
                 width: 280,
                 height: 64,
                 text: "creative strategy",
-                offset: 660,
-                fallHeight: 50,
+                offset: 600,
+                fallHeight: 150,
               },
             ]}
           />
