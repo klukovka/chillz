e0c9f97c8f90cef987c7be6f1ccc557bb055139b
Yuliia Koba <ykoba@clipmoney.com>
2025-03-10 20:05:47 +0200

    added code image

--- src/pages/contact-us/components/ContactUsFormBackground.jsx
+++ src/pages/contact-us/components/ContactUsFormBackground.jsx
@@ -1,11 +1,17 @@
+import formCode from "../../../assets/form_code.svg";
 import classes from "./ContactUsFormBackground.module.css";
 
 export default function ContactUsFormBackground({ children }) {
   return (
-    <div className={classes["form-background"]}>
-      <div className={classes["background"]}>{children}</div>
-      <div className={classes["personal-data"]}>
-        <p>We will not spam or share your personal data.</p>
+    <div className={classes["stack"]}>
+      <div className={classes["form-background"]}>
+        <div className={classes["background"]}>{children}</div>
+        <div className={classes["personal-data"]}>
+          <p>We will not spam or share your personal data.</p>
+        </div>
+      </div>
+      <div className={classes["code"]}>
+        <img src={formCode} />
       </div>
     </div>
   );
--- src/pages/contact-us/pages/InfluContactUsPage.jsx
+++ src/pages/contact-us/pages/InfluContactUsPage.jsx
@@ -1,5 +1,130 @@
+import { useInput } from "../../../hooks/useInput";
+import { isEmail, isNotEmpty, isNotEmptyList } from "../../../util/validation";
+import ContactUsDivider from "../components/ContactUsDivider";
+import ContactUsCheckboxSet from "../components/ContactUseCheckboxSet";
 import ContactUsFormBackground from "../components/ContactUsFormBackground";
+import ContactUsInput from "../components/ContactUsInput";
 
 export default function InfluContactUsPage() {
-  return <ContactUsFormBackground>Influ</ContactUsFormBackground>;
+  const nameField = useInput("", isNotEmpty);
+  const emailField = useInput(
+    "",
+    (value) => isNotEmpty(value) ?? isEmail(value)
+  );
+  const instalinkField = useInput("", isNotEmpty);
+  const servicesField = useInput([], isNotEmptyList);
+  const commentField = useInput("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const services = fd.getAll("services");
+
+    const data = Object.fromEntries(fd.entries());
+    data.services = services;
+
+    nameField.onSubmit();
+    emailField.onSubmit();
+    instalinkField.onSubmit();
+    servicesField.onSubmit();
+    commentField.onSubmit();
+
+    console.log(data);
+  }
+
+  return (
+    <ContactUsFormBackground>
+      <p
+        style={{
+          padding: 0,
+          margin: 0,
+          fontSize: "2rem",
+          color: "black",
+          fontWeight: "600",
+        }}
+      >
+        Tell us what's on your mind, and we'll make it happen
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <ContactUsDivider bold>
+          <ContactUsInput
+            label="your name"
+            id="name"
+            name="name"
+            value={nameField.value}
+            error={nameField.errorText}
+            onChange={nameField.handleChange}
+          />
+          <ContactUsInput
+            label="email"
+            id="email"
+            name="email"
+            type="email"
+            value={emailField.value}
+            error={emailField.errorText}
+            onChange={emailField.handleChange}
+          />
+        </ContactUsDivider>
+        <ContactUsDivider>
+          <ContactUsInput
+            label="your channel link"
+            id="instalink"
+            name="instalink"
+            value={instalinkField.value}
+            error={instalinkField.errorText}
+            onChange={instalinkField.handleChange}
+          />
+        </ContactUsDivider>
+        <ContactUsDivider>
+          <ContactUsCheckboxSet
+            name={"services"}
+            label="choose a service"
+            value={servicesField.value}
+            error={servicesField.errorText}
+            onChange={(value) =>
+              servicesField.handleChange({ target: { value: value } })
+            }
+            items={[
+              {
+                id: "sm_strategy",
+                value: "sm_strategy",
+                label: "SM strategy",
+              },
+              {
+                id: "promo",
+                value: "promo",
+                label: "promo",
+              },
+              {
+                id: "counsalting",
+                value: "counsalting",
+                label: "counsalting",
+              },
+              {
+                id: "visual",
+                value: "visual",
+                label: "visual",
+              },
+            ]}
+          />
+        </ContactUsDivider>
+        <ContactUsDivider>
+          <ContactUsInput
+            label="comment (optional)"
+            id="comment"
+            name="comment"
+            textarea
+            value={commentField.value}
+            error={commentField.errorText}
+            onChange={commentField.handleChange}
+          />
+        </ContactUsDivider>
+        <button type="submit" className="button">
+          Sign up
+        </button>
+      </form>
+    </ContactUsFormBackground>
+  );
 }
