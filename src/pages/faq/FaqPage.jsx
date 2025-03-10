4a89903b7dd634e3e24ae36b0ac229c627d18b71
Yuliia Koba <ykoba@clipmoney.com>
2025-03-10 14:35:20 +0200

    added footer

--- src/pages/faq/FaqPage.jsx
+++ src/pages/faq/FaqPage.jsx
@@ -1,7 +1,26 @@
+import PinAnimation from "../../components/PinAnimation";
+import RoundedArrow from "../../components/RoundedArrow";
+import WidthDimensionsContainer from "../../components/WidthDimensionsContainer";
+import classes from "./FaqPage.module.css";
+
 export default function FaqPage() {
   return (
-    <>
-      <h1>FaqPage</h1>
-    </>
+    <WidthDimensionsContainer>
+      <div className={classes["faq-page"]}>
+        <PinAnimation>
+          <h1 style={{ textAlign: "center" }}>any questions?</h1>
+          <h1 style={{ textAlign: "center" }}>
+            <span style={{ color: "#9B4DFF" }}>we can help you!</span>
+          </h1>
+        </PinAnimation>
+        <PinAnimation>
+          <h3 className={classes["footer"]}>
+            Still have questions? <span>Let’s chat! Drop us a message</span>,
+            and we’ll clear things up<span> </span>
+            <RoundedArrow className={classes["arrow"]} />
+          </h3>
+        </PinAnimation>
+      </div>
+    </WidthDimensionsContainer>
   );
 }
