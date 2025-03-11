c1bd6d01cc845728d9af9ebd99581a79adcb9026
Yuliia Koba <ykoba@clipmoney.com>
2025-03-11 17:16:00 +0200

    add titles to ArticlesPage

--- src/pages/articles/ArticlesPage.jsx
+++ src/pages/articles/ArticlesPage.jsx
@@ -1,7 +1,27 @@
+import PinAnimation from "../../components/PinAnimation";
+import RoundedArrow from "../../components/RoundedArrow";
+import WidthDimensionsContainer from "../../components/WidthDimensionsContainer";
+import classes from "./ArticlesPage.module.css";
+
 export default function ArticlesPage() {
   return (
-    <>
-      <h1>ArticlesPage</h1>
-    </>
+    <WidthDimensionsContainer>
+      <div className={classes["articles-page"]}>
+        <PinAnimation>
+          <h1 style={{ textAlign: "center" }}>
+            <span style={{ color: "#7DC8F7" }}>stay ahead </span>of the social
+            tide
+          </h1>
+        </PinAnimation>
+
+        <PinAnimation>
+          <h3 className={classes["footer"]}>
+            🌊Want the <span>freshest</span> marketing tips?
+            <span>Subscribe</span> and stay ahead of the tide!<span> </span>
+            <RoundedArrow className={classes["arrow"]} />
+          </h3>
+        </PinAnimation>
+      </div>
+    </WidthDimensionsContainer>
   );
 }
