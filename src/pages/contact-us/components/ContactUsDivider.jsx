dbd85e0943a8a93bbd98f4110e087f361ed4b9a8
Yuliia Koba <ykoba@clipmoney.com>
2025-03-12 22:21:07 +0200

    update ContactUsFormBackground

--- src/pages/contact-us/components/ContactUsDivider.jsx
+++ src/pages/contact-us/components/ContactUsDivider.jsx
@@ -1,11 +1,34 @@
+import { useMediaQuery } from "@mui/material";
+import React from "react";
 import classes from "./ContactUsDivider.module.css";
 
 export default function ContactUsDivider({ children, bold }) {
+  const matches = useMediaQuery("(min-width:1020px)");
+
+  if (matches) {
+    return (
+      <div
+        className={classes["divider"] + " " + `${bold ? classes["bold"] : ""}`}
+      >
+        {children}
+      </div>
+    );
+  }
+
   return (
-    <div
-      className={classes["divider"] + " " + `${bold ? classes["bold"] : ""}`}
-    >
-      {children}
-    </div>
+    <>
+      {React.Children.toArray(children).map((child, index) => (
+        <div
+          key={`ContactUsDivider${index}`}
+          className={
+            classes["divider"] +
+            " " +
+            `${bold && index == 0 ? classes["bold"] : ""}`
+          }
+        >
+          {child}
+        </div>
+      ))}
+    </>
   );
 }
