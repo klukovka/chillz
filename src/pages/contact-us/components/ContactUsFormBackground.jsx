ee43b8aeeec81fe5d7d4595bb255199aa74fd648
Yuliia Koba <ykoba@clipmoney.com>
2025-03-12 22:54:21 +0200

    fixed ContactUsFormBackground

--- src/pages/contact-us/components/ContactUsFormBackground.jsx
+++ src/pages/contact-us/components/ContactUsFormBackground.jsx
@@ -1,4 +1,5 @@
-import formCode from "../../../assets/form_code.svg";
+import formCodeHorizontal from "../../../assets/form_code_horizontal.svg";
+import formCodeVerical from "../../../assets/form_code_vertical.svg";
 import classes from "./ContactUsFormBackground.module.css";
 
 export default function ContactUsFormBackground({ children }) {
@@ -8,10 +9,13 @@ export default function ContactUsFormBackground({ children }) {
         <div className={classes["background"]}>{children}</div>
         <div className={classes["personal-data"]}>
           <p>We will not spam or share your personal data.</p>
+          <div className={classes["code-horizontal"]}>
+            <img src={formCodeHorizontal} />
+          </div>
         </div>
       </div>
-      <div className={classes["code"]}>
-        <img src={formCode} />
+      <div className={classes["code-vertical"]}>
+        <img src={formCodeVerical} />
       </div>
     </div>
   );
