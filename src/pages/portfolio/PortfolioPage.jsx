cde8a161600c633e0ad884ec2acb208bdee1e7bd
Yuliia Koba <ykoba@clipmoney.com>
2025-03-19 18:06:15 +0200

    add text to PortfolioPage

--- src/pages/portfolio/PortfolioPage.jsx
+++ src/pages/portfolio/PortfolioPage.jsx
@@ -1,7 +1,32 @@
+import PinAnimation from "../../components/PinAnimation";
+import RoundedArrow from "../../components/RoundedArrow";
+import WidthDimensionsContainer from "../../components/WidthDimensionsContainer";
+import classes from "./PortfolioPage.module.css";
+
 export default function PortfolioPage() {
   return (
-    <>
-      <h1>PortfolioPage</h1>
-    </>
+    <WidthDimensionsContainer>
+      <div className={classes["portfolio-page"]}>
+        <PinAnimation>
+          <h1 style={{ textAlign: "center" }}>creative</h1>
+          <h1 style={{ textAlign: "center" }}>
+            cr<span style={{ color: "#E84E31" }}>hub</span>
+          </h1>
+        </PinAnimation>
+        <PinAnimation>
+          <p>
+            Take a peek into our creative world. From bold strategies to
+            seamless execution, we craft solutions that drive real results
+          </p>
+        </PinAnimation>
+        <PinAnimation>
+          <h3 className={classes["footer"]}>
+            🦀<span>Drop us a line</span>, and we'll hook you uo with a{" "}
+            <span>crab-solutely brilliant plan! </span>
+            <RoundedArrow className={classes["arrow"]} />
+          </h3>
+        </PinAnimation>
+      </div>
+    </WidthDimensionsContainer>
   );
 }
