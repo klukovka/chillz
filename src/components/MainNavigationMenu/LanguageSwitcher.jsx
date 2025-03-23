7aa2d68453ecee965539dbc4100721d04f125284
Yuliia Koba <ykoba@clipmoney.com>
2025-03-23 16:32:22 +0200

    todo

--- src/components/MainNavigationMenu/LanguageSwitcher.jsx
+++ src/components/MainNavigationMenu/LanguageSwitcher.jsx
@@ -1,6 +1,6 @@
 import classes from "./MainNavigationBar.module.css";
 
-//TODO: Implement localization
+//TODO: Implement localization and animation
 export default function LanguageSwitcher() {
   return (
     <li className={classes["main-nav__item"] + " " + classes.language}>EN</li>
