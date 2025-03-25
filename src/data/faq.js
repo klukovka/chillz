78495eb06a4bc1db12d589e152728ddfcecb83f8
Yuliia Koba <ykoba@clipmoney.com>
2025-03-25 17:39:43 +0200

    .

--- src/data/faq.js
+++ src/data/faq.js
@@ -59,7 +59,7 @@ const BILLING_FAQ = {
   name: "billing",
   content: [
     {
-      question: "billing question",
+      question: "billing question!",
       answer:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
