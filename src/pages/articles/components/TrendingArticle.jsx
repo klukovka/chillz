8f6e7850182474921215da2631025e469ec70399
Yuliia Koba <ykoba@clipmoney.com>
2025-03-30 15:53:55 +0300

    TrendingArticle localization

--- src/pages/articles/components/TrendingArticle.jsx
+++ src/pages/articles/components/TrendingArticle.jsx
@@ -1,8 +1,10 @@
+import { useTranslation } from "react-i18next";
 import { NavLink, useLocation, useSearchParams } from "react-router-dom";
 import ArticleCategory from "./ArticleCategory";
 import classes from "./TrendingArticle.module.css";
 
 export default function TrendingArticle({ article }) {
+  const { t } = useTranslation();
   const location = useLocation();
   const [searchParams] = useSearchParams();
   const page = searchParams.get("page");
@@ -14,7 +16,10 @@ export default function TrendingArticle({ article }) {
       <img src={article.imageUrl} />
       <div className={classes["additional-info"]}>
         <p>
-          Reading time <span>{article.readingTime} min.</span>
+          {t("reading_time")}{" "}
+          <span>
+            {article.readingTime} {t("min")}.
+          </span>
         </p>
         <ArticleCategory category={article.category} />
       </div>
