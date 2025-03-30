bf3f4e1438a13de48f22b94e1854106470971ea2
Yuliia Koba <ykoba@clipmoney.com>
2025-03-30 16:25:17 +0300

    ContactUsPage localizations

--- src/pages/contact-us/ContactUsPage.jsx
+++ src/pages/contact-us/ContactUsPage.jsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
 import PinAnimation from "../../components/PinAnimation";
 import WidthDimensionsContainer from "../../components/WidthDimensionsContainer";
@@ -5,19 +6,25 @@ import ContactUsSegment from "./components/ContactUsSegment";
 import classes from "./ContactUsPage.module.css";
 
 export default function ContactUsPage() {
+  const { t } = useTranslation();
   return (
     <WidthDimensionsContainer>
       <PinAnimation>
         <h1 style={{ textAlign: "center" }}>
-          your next <span style={{ color: "#EF4B8D" }}>big move</span> starts
-          here!
+          {t("contact_us_page_title_p1")}{" "}
+          <span style={{ color: "#EF4B8D" }}>
+            {t("contact_us_page_title_highlight")}
+          </span>{" "}
+          {t("contact_us_page_title_p2")}
         </h1>
       </PinAnimation>
       <PinAnimation>
         <div className={classes["tabs"]}>
-          <ContactUsSegment to={"influencers"}>influ</ContactUsSegment>
+          <ContactUsSegment to={"influencers"}>
+            {t("influ").toLowerCase()}
+          </ContactUsSegment>
           <ContactUsSegment to={"brands"} end>
-            brands
+            {t("brands").toLowerCase()}
           </ContactUsSegment>
         </div>
       </PinAnimation>
