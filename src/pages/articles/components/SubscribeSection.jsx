c0e7b1cd4327e03b94defeb1847ad60cdb6124ca
Yuliia Koba <ykoba@clipmoney.com>
2025-03-30 15:52:56 +0300

    SubscribeSection localizations

--- src/pages/articles/components/SubscribeSection.jsx
+++ src/pages/articles/components/SubscribeSection.jsx
@@ -1,14 +1,22 @@
+import { useTranslation } from "react-i18next";
 import RoundedArrow from "../../../components/RoundedArrow";
 import classes from "./SubscribeSection.module.css";
 
 export default function SubscribeSection() {
+  const { t } = useTranslation();
   return (
     <div className={classes.subsribe}>
       <h3 className={classes["subsribe-header"]}>
-        <span style={{ color: "#9B4DFF" }}>Subscribe</span> and{" "}
-        <span style={{ color: "#7DC8F7" }}>stay ahead</span> of the tide!
+        <span style={{ color: "#9B4DFF" }}>
+          {t("subscribe_stay_ahead_first_hightlight")}
+        </span>{" "}
+        {t("subscribe_stay_ahead_first_label")}{" "}
+        <span style={{ color: "#7DC8F7" }}>
+          {t("subscribe_stay_ahead_second_hightlight")}
+        </span>{" "}
+        {t("subscribe_stay_ahead_second_label")}
       </h3>
-      <p>Your e-mail:</p>
+      <p>{t("your_email")}:</p>
       <div className={classes.control}>
         <input placeholder="crayby@chiillz.com" type="email" />
         <RoundedArrow className={classes["arrow"]} height={29} width={30} />
