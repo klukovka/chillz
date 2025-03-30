006712a4b8359c1d537d003f5f4b2d97e9212cc2
Yuliia Koba <ykoba@clipmoney.com>
2025-03-30 14:36:04 +0300

    app bar localizations

--- src/components/MainNavigationMenu/MainNavigationMenuItems.jsx
+++ src/components/MainNavigationMenu/MainNavigationMenuItems.jsx
@@ -1,37 +1,39 @@
+import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "./LanguageSwitcher";
 import classes from "./MainNavigationBar.module.css";
 import MainNavigationBarItem from "./MainNavigationBarItem";
 
 export default function MainNavigationBarItems({ id, onClick }) {
+  const { t } = useTranslation();
   return (
     <ul className={classes["main-nav__items"]} id={id}>
       <MainNavigationBarItem to="" end onClick={onClick}>
-        about
+        {t("about").toLowerCase()}
       </MainNavigationBarItem>
       <MainNavigationBarItem to="services" onClick={onClick}>
-        services
+        {t("services").toLowerCase()}
       </MainNavigationBarItem>
       <MainNavigationBarItem to="portfolio" onClick={onClick}>
-        portfolio
+        {t("portfolio").toLowerCase()}
       </MainNavigationBarItem>
       <MainNavigationBarItem to="articles" onClick={onClick}>
-        articles
+        {t("articles").toLowerCase()}
       </MainNavigationBarItem>
       <MainNavigationBarItem to="faq" onClick={onClick}>
-        FAQ
+        {t("faq")}
       </MainNavigationBarItem>
       <MainNavigationBarItem
         to="contact-us/brands"
         id={classes["contact-us-button"]}
       >
-        <button>contact us</button>
+        <button>{t("contact_us").toLowerCase()}</button>
       </MainNavigationBarItem>
       <MainNavigationBarItem
         onClick={onClick}
         to="contact-us/brands"
         id={classes["contact-us-link"]}
       >
-        contact us
+        {t("contact_us").toLowerCase()}
       </MainNavigationBarItem>
       <LanguageSwitcher />
     </ul>
