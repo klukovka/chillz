bfb46d3d6ba7e6d28ae135a85f14ef70286024b7
Yuliia Koba <ykoba@clipmoney.com>
2025-03-30 15:22:01 +0300

    SushiCraby localizations

--- src/pages/about/components/SushiCraby.jsx
+++ src/pages/about/components/SushiCraby.jsx
@@ -1,19 +1,24 @@
+import { useTranslation } from "react-i18next";
 import craby from "../../../assets/craby_sushi.svg";
 import AboutFlexContainer from "./AboutFlexContainer";
 import classes from "./SushiCraby.module.css";
 
 export default function SushiCraby() {
+  const { t } = useTranslation();
   return (
     <AboutFlexContainer className={classes["sushi-craby-section"]}>
       <section className={classes["craby-section"]}>
         <h2>
-          <span style={{ color: "#FFD766" }}>fresh</span> ideas
+          <span style={{ color: "#FFD766" }}>{t("fresh_ideas_highlight")}</span>{" "}
+          {t("fresh_ideas_title")}
         </h2>
         <h2>
-          <span style={{ color: "#9B4DFF" }}>real</span> impact
+          <span style={{ color: "#9B4DFF" }}>{t("real_impact_highlight")}</span>{" "}
+          {t("real_impact_title")}
         </h2>
         <h2>
-          <span style={{ color: "#EF4B8D" }}>zero</span> stress
+          <span style={{ color: "#EF4B8D" }}>{t("zero_stress_highlight")}</span>{" "}
+          {t("zero_stress_title")}
         </h2>
       </section>
       <img src={craby} alt="Chillz Logo" className={classes["rotated-craby"]} />
