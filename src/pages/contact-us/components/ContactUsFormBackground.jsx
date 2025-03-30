5d22911cffe7a67f799af2d257e8824646612d8a
Yuliia Koba <ykoba@clipmoney.com>
2025-03-30 16:04:17 +0300

    ContactUsFormBackground localizations

--- src/pages/contact-us/components/ContactUsFormBackground.jsx
+++ src/pages/contact-us/components/ContactUsFormBackground.jsx
@@ -1,14 +1,16 @@
+import { useTranslation } from "react-i18next";
 import formCodeHorizontal from "../../../assets/form_code_horizontal.svg";
 import formCodeVerical from "../../../assets/form_code_vertical.svg";
 import classes from "./ContactUsFormBackground.module.css";
 
 export default function ContactUsFormBackground({ children }) {
+  const { t } = useTranslation();
   return (
     <div className={classes["stack"]}>
       <div className={classes["form-background"]}>
         <div className={classes["background"]}>{children}</div>
         <div className={classes["personal-data"]}>
-          <p>We will not spam or share your personal data.</p>
+          <p>{t("not_spam_and_share_data")}</p>
           <div className={classes["code-horizontal"]}>
             <img src={formCodeHorizontal} />
           </div>
