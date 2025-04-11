fe554ce4ab8a7d4d4a6dc811dfc191f9ef0b58b1
Yuliia Koba <ykoba@clipmoney.com>
2025-04-11 17:11:39 +0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import logo from "../assets/craby_white.svg";
 import facebook from "../assets/facebook.svg";
@@ -9,50 +10,78 @@ import FooterNavigationBarItem from "./FooterNavigationItem";
 
 export default function Footer() {
   const { t } = useTranslation();
+  const matches = useMediaQuery("(min-width:750px)");
+
+  const socialMedia = (
+    <div className={classes["social-media"]}>
+      <span>{t("follow_for_more").toUpperCase()}</span>
+      <div>
+        <img src={instagram} alt="Instagram" />
+        <img src={facebook} alt="Facebook" />
+        <img src={telegram} alt="Telegram" />
+        <img src={youtube} alt="Youtube" />
+      </div>
+    </div>
+  );
+
+  const privacyPolicy = (
+    <>
+      <p>
+        {t("copyright").toLowerCase()} © {new Date().getFullYear()}
+        {` chiillz. `} {t("all_rights_reserved").toLowerCase()}
+      </p>
+      <div className={classes["privacy-policy"]}>
+        <a href="/">{t("privacy_policy").toLowerCase()}</a>
+        <a href="/">{t("terms_and_conditions")}</a>
+      </div>
+    </>
+  );
+
+  const navigation = (
+    <div className={classes["navigation"]}>
+      <nav>
+        <ul className={classes["footer-nav__items"]}>
+          <FooterNavigationBarItem to="">
+            {t("about").toLowerCase()}
+          </FooterNavigationBarItem>
+          <FooterNavigationBarItem to="articles">
+            {t("articles").toLowerCase()}
+          </FooterNavigationBarItem>
+          <FooterNavigationBarItem to="services">
+            {t("services").toLowerCase()}
+          </FooterNavigationBarItem>
+          <FooterNavigationBarItem to="faq">FAQ</FooterNavigationBarItem>
+          <FooterNavigationBarItem to="portfolio">
+            {t("portfolio").toLowerCase()}
+          </FooterNavigationBarItem>
+          <FooterNavigationBarItem to="contact-us">
+            {t("contact_us").toLowerCase()}
+          </FooterNavigationBarItem>
+        </ul>
+      </nav>
+    </div>
+  );
+
+  if (matches) {
+    return (
+      <footer className={classes["main-footer"]}>
+        <div>
+          <img src={logo} alt="Chillz logo" className={classes["logo-image"]} />
+          {socialMedia}
+          {privacyPolicy}
+        </div>
+        {navigation}
+      </footer>
+    );
+  }
+
   return (
     <footer className={classes["main-footer"]}>
       <div>
         <img src={logo} alt="Chillz logo" className={classes["logo-image"]} />
-        <div className={classes["social-media"]}>
-          <span>{t("follow_for_more").toUpperCase()}</span>
-          <div>
-            <img src={instagram} alt="Instagram" />
-            <img src={facebook} alt="Facebook" />
-            <img src={telegram} alt="Telegram" />
-            <img src={youtube} alt="Youtube" />
-          </div>
-        </div>
-        <p>
-          {t("copyright").toLowerCase()} © {new Date().getFullYear()}
-          {` chiillz. `} {t("all_rights_reserved").toLowerCase()}
-        </p>
-        <div className={classes["privacy-policy"]}>
-          <a href="/">{t("privacy_policy").toLowerCase()}</a>
-          <a href="/">{t("terms_and_conditions")}</a>
-        </div>
-      </div>
-      <div className={classes["navigation"]}>
-        <nav>
-          <ul className={classes["footer-nav__items"]}>
-            <FooterNavigationBarItem to="">
-              {" "}
-              {t("about").toLowerCase()}
-            </FooterNavigationBarItem>
-            <FooterNavigationBarItem to="articles">
-              {t("articles").toLowerCase()}
-            </FooterNavigationBarItem>
-            <FooterNavigationBarItem to="services">
-              {t("services").toLowerCase()}
-            </FooterNavigationBarItem>
-            <FooterNavigationBarItem to="faq">FAQ</FooterNavigationBarItem>
-            <FooterNavigationBarItem to="portfolio">
-              {t("portfolio").toLowerCase()}
-            </FooterNavigationBarItem>
-            <FooterNavigationBarItem to="contact-us">
-              {t("contact_us").toLowerCase()}
-            </FooterNavigationBarItem>
-          </ul>
-        </nav>
+        {navigation}
+        {socialMedia}
+        {privacyPolicy}
       </div>
     </footer>
   );
